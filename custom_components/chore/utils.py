398794805b1f197b44c1d080463e9a2e0267ff3c
Volodymyr Honchar <vhonchar94@gmail.com>
2025-05-12 19:12:07 -0700

    Install ruff for linking and checkstyle. Fixed all formatting rules

--- custom_components/chore/utils.py
+++ custom_components/chore/utils.py
@@ -1,19 +1,19 @@
-"""Utility functions"""
+"""Utility functions."""
 import inspect
-from datetime import datetime
+from datetime import date, datetime
 from typing import Any
 from zoneinfo import ZoneInfo
 
 from homeassistant.core import HomeAssistant
 
 
-def filter_kwargs_for_init(cls, kwargs: dict[str, Any], skip_additionally: list[str] = ()):
-    """filter provided dictionary to leave only those keys, which are met in constructor of the cls"""
-
+def filter_kwargs_for_init(cls: type, kwargs: dict[str, Any], skip_additionally: list[str] = ()) -> dict[str, Any]:
+    """Filter provided dictionary to leave only those keys, which are met in constructor of the cls."""
     sig = inspect.signature(cls.__init__)
-    valid_params = set(sig.parameters) - {'self'}
+    valid_params = set(sig.parameters) - {"self"}
     return {k: v for k, v in kwargs.items() if k in valid_params and k not in skip_additionally}
 
-def ha_today(hass: HomeAssistant):
+def ha_today(hass: HomeAssistant) -> date:
+    """Get today's date in time zone configured in HomeAssistant."""
     ha_tz = ZoneInfo(hass.config.time_zone)
     return datetime.now(ha_tz).date()
